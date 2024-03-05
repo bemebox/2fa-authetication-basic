86ff0e64ab3dc3c4b257a835156d39526923b1e8
bemebox <bemebox@gmail.com>
2024-03-05 09:47:03 +0000

    add talwind css to the project

--- 2FA-Login/src/App.vue
+++ 2FA-Login/src/App.vue
@@ -1,6 +1,11 @@
-<template>
-  <RouterView />
-</template>
 <script setup lang="ts">
   import { RouterLink, RouterView } from 'vue-router'
 </script>
+
+<template>
+  <div class="flex flex-col min-h-screen container mx-auto">
+    <main class="p-8">
+      <RouterView />
+    </main>
+  </div>
+</template>
